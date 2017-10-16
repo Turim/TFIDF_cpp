27150337dce210e508570ba01be8e337eae2cd15
Alexander Enaldiev <Alexander.Enaldiev@gmail.com>
2017-10-16 15:26:57 +0300

    code has been unified to tfidf.h

---
+++ tfidf.h
@@ -0,0 +1,139 @@
+#ifndef TFIDF__TFIDF_H
+#define TFIDF__TFIDF_H
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <string>
+#include <vector>
+
+class tfidf {
+private:
+	std::vector<std::vector<double>> dataMat; // converted bag of words matrix
+	unsigned int nrow; // matrix row number
+	unsigned int ncol; // matrix column number
+	std::vector<std::vector<std::string>> rawDataSet; // raw data
+	std::vector<std::string> vocabList; // all terms
+	std::vector<int> numOfTerms; // used in tf calculation
+
+private:
+	void createVocabList();
+	inline std::vector<double> bagOfWords2VecMN(const std::vector<std::string> & inputSet);
+	void vec2mat();
+	inline std::vector<double> vec_sum(const std::vector<double>& a, const std::vector<double>& b);
+	void calMat();
+
+
+public:
+	std::vector<std::vector<double>> weightMat; // TF-IDF weighting matrix
+	tfidf(std::vector<std::vector<std::string>> & input) :rawDataSet(input)
+	{
+		calMat();
+	}
+
+	tfidf(std::vector<std::vector<std::string>> & input, const std::vector<std::string>& vocabList)
+		: rawDataSet(input), vocabList(vocabList)
+	{
+		calMat();
+	}
+};
+
+inline void tfidf::createVocabList()
+{
+	assert(vocabList.empty());
+	std::set<std::string> vocabListSet;
+	for (std::vector<std::string> document : rawDataSet)
+	{
+		for (std::string word : document)
+			vocabListSet.insert(word);
+	}
+	std::copy(vocabListSet.begin(), vocabListSet.end(), std::back_inserter(vocabList));
+}
+
+inline std::vector<double> tfidf::bagOfWords2VecMN(const std::vector<std::string> & inputSet)
+{
+	assert(!vocabList.empty());
+	std::vector<double> returnVec(vocabList.size(), 0);
+	for (std::string word : inputSet)
+	{
+		size_t idx = std::find(vocabList.begin(), vocabList.end(), word) - vocabList.begin();
+		if (idx == vocabList.size())
+			std::cout << "word: " << word << "not found" << std::endl;
+		else
+			returnVec.at(idx) += 1;
+	}
+	return returnVec;
+}
+
+inline void tfidf::vec2mat()
+{
+	int cnt(0);
+	for (auto it = rawDataSet.begin(); it != rawDataSet.end(); ++it)
+	{
+		cnt++;
+		std::cout << cnt << "\r";
+		std::cout.flush();
+		dataMat.push_back(bagOfWords2VecMN(*it));
+		numOfTerms.push_back(it->size());
+		it->clear();
+	}
+	std::cout << std::endl;
+	ncol = dataMat[0].size();
+	nrow = dataMat.size();
+	rawDataSet.clear(); // release memory
+}
+
+inline std::vector<double> tfidf::vec_sum(const std::vector<double>& a, const std::vector<double>& b)
+{
+	assert(a.size() == b.size());
+	std::vector<double> result;
+	result.reserve(a.size());
+	std::transform(a.begin(), a.end(), b.begin(),
+		std::back_inserter(result), std::plus<double>());
+	return result;
+}
+
+inline void tfidf::calMat()
+{
+	if (vocabList.empty())
+		createVocabList();
+	vec2mat();
+
+	std::vector<std::vector<double>> dataMat2(dataMat);
+	std::vector<double> termCount;
+	termCount.resize(ncol);
+
+	for (unsigned int i = 0; i != nrow; ++i)
+	{
+		for (unsigned int j = 0; j != ncol; ++j)
+		{
+			if (dataMat2[i][j] > 1) // only keep 1 and 0
+				dataMat2[i][j] = 1;
+		}
+		termCount = vec_sum(termCount, dataMat2[i]); // no. of doc. each term appears
+	}
+	dataMat2.clear(); //release
+
+	std::vector<double> row_vec;
+	for (unsigned int i = 0; i != nrow; ++i)
+	{
+		for (unsigned int j = 0; j != ncol; ++j)
+		{
+			double tf = dataMat[i][j] / numOfTerms[i];
+			double idf = log((double)nrow / (termCount[j]));
+			row_vec.push_back(tf * idf); // TF-IDF equation
+		}
+		weightMat.push_back(row_vec);
+		row_vec.clear();
+	}
+	nrow = weightMat.size();
+	ncol = weightMat[0].size();
+	std::cout << "calcMat: nrow = " << nrow << "; ncol = " << ncol << std::endl;
+}
+
+
+
+
+#endif // TFIDF__TFIDF_H
--- tfidf_classify.cpp
+++ tfidf_classify.cpp
@@ -1,3 +1,5 @@
+#include "tfidf.h"
+
 #include <cassert>
 #include <iostream>
 #include <fstream>
@@ -43,130 +45,6 @@
 #endif
 
 using namespace std;
-
-class tfidf {
-private:
-	std::vector<std::vector<double>> dataMat; // converted bag of words matrix
-	unsigned int nrow; // matrix row number
-	unsigned int ncol; // matrix column number
-	std::vector<std::vector<std::string>> rawDataSet; // raw data
-	std::vector<std::string> vocabList; // all terms
-	std::vector<int> numOfTerms; // used in tf calculation
-	
-	void createVocabList();
-	inline std::vector<double> bagOfWords2VecMN(const std::vector<std::string> & inputSet);
-	void vec2mat();
-	inline std::vector<double> vec_sum(const std::vector<double>& a, const std::vector<double>& b);
-	
-
-public:
-	std::vector<std::vector<double>> weightMat; // TF-IDF weighting matrix
-	tfidf(std::vector<std::vector<std::string>> & input):rawDataSet(input)
-	{
-#if 0
-		calMat();
-#endif
-	}
-
-	void setVocabList(std::vector<std::string>&& newVocabList)
-	{
-		vocabList = newVocabList;
-	}
-#if 1
-	void calMat();
-#endif
-};
-
-void tfidf::createVocabList()
-{
-	std::set<std::string> vocabListSet;
-	for (std::vector<std::string> document : rawDataSet)
-	{
-		for (std::string word : document)
-			vocabListSet.insert(word);
-	}
-	std::copy(vocabListSet.begin(), vocabListSet.end(), std::back_inserter(vocabList));
-}
-
-inline std::vector<double> tfidf::bagOfWords2VecMN(const std::vector<std::string> & inputSet)
-{
-	std::vector<double> returnVec(vocabList.size(), 0);
-	for (std::string word : inputSet)
-	{
-		size_t idx = std::find(vocabList.begin(), vocabList.end(), word) - vocabList.begin();
-		if (idx == vocabList.size())
-			cout << "word: " << word << "not found" << endl;
-		else
-			returnVec.at(idx) += 1;
-	}
-	return returnVec;
-}
-
-void tfidf::vec2mat()
-{
-	int cnt(0);
-	for (auto it = rawDataSet.begin(); it != rawDataSet.end(); ++ it)
-	{
-		cnt ++;
-		cout << cnt << "\r";
-		std::cout.flush();
-		dataMat.push_back(bagOfWords2VecMN(*it));
-		numOfTerms.push_back(it->size());
-		it->clear();
-	}
-	cout << endl;
-	ncol = dataMat[0].size();
-	nrow = dataMat.size();
-	rawDataSet.clear(); // release memory
-}
-
-inline std::vector<double> tfidf::vec_sum(const std::vector<double>& a, const std::vector<double>& b)
-{
-    assert(a.size() == b.size());
-    std::vector<double> result;
-    result.reserve(a.size());
-    std::transform(a.begin(), a.end(), b.begin(), 
-                   std::back_inserter(result), std::plus<double>());
-    return result;
-}
-
-void tfidf::calMat()
-{
-	if (vocabList.empty())
-		createVocabList();
-	vec2mat();
-
-	std::vector<std::vector<double>> dataMat2(dataMat);
-	std::vector<double> termCount;
-	termCount.resize(ncol);
-
-	for (unsigned int i = 0; i != nrow; ++i)
-	{
-		for (unsigned int j = 0; j != ncol; ++j)
-		{
-			if (dataMat2[i][j] > 1) // only keep 1 and 0
-				dataMat2[i][j] = 1;
-		}
-		termCount = vec_sum(termCount, dataMat2[i]); // no. of doc. each term appears
-	}
-	dataMat2.clear(); //release
-
-	std::vector<double> row_vec;
-	for (unsigned int i = 0; i != nrow; ++i)
-	{
-		for (unsigned int j = 0; j != ncol; ++j)
-		{
-			double tf = dataMat[i][j] / numOfTerms[i];
-			double idf = log((double)nrow / (termCount[j]));
-			row_vec.push_back(tf * idf); // TF-IDF equation
-		}
-		weightMat.push_back(row_vec);
-		row_vec.clear();
-	}
-	nrow = weightMat.size();
-	ncol = weightMat[0].size();
-	cout << "calcMat: nrow = " << nrow << "; ncol = " << ncol << endl;
-}
 
 // when , is inside of " then it changes to ^
 template<typename iterator>
@@ -390,10 +268,7 @@ try
 
 		// train 'em all!!
 		Shuffle(inputData, responsesVec[classToTrain]);
-		tfidf data(inputData);
-		if (!vocabulary.empty()) // 
-			data.setVocabList(std::move(vocabulary));
-		data.calMat();
+		tfidf data(inputData, vocabulary);
 		vector<vector<double>> trainDataVec(data.weightMat.begin(), data.weightMat.begin() + trainCount);
 		vector<int> trainResponsesVec(responsesVec[classToTrain].begin(), responsesVec[classToTrain].begin() + trainCount);
 		const Mat trainDataVecProxy = TfIdfWeights2Mat(trainDataVec);
@@ -402,10 +277,7 @@ try
 	}
 
 	// Stage III. Predict
-	tfidf data(inputData);
-	if (!vocabulary.empty()) // 
-		data.setVocabList(std::move(vocabulary));
-	data.calMat();
+	tfidf data(inputData, vocabulary);
 	vector<vector<double>> testDataVec(data.weightMat.begin() + trainCount, data.weightMat.end());
 	std::default_random_engine generator(time(NULL));
 	assert(ClassesToTrain > 0);
